88d6de7e1670c7acd93c570e1437652b75c25e6e
24f2000042-a11y <24f2000042@ds.study.iitm.ac.in>
2026-05-21 11:37:58 +0530

    add backend/routes/itemRoutes.js

---
+++ backend/routes/itemRoutes.js
@@ -0,0 +1,1 @@
+const express = require('express'); const { getItems, createItem } = require('../controllers/itemController'); const router = express.Router(); router.get('/', getItems); router.post('/', createItem); module.exports = router;
